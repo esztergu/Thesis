623b514ac62b1d18d737d0e9ea938dcb20de091d
Eszter <gulyaseszter.ge@gmail.com>
2024-11-24 17:22:46 +0100

    Similiratity search fixed, English name retrival

--- weight_compare.py
+++ weight_compare.py
@@ -2,6 +2,9 @@ import sparsechem as sc
 import torch
 import pandas as pd
 import numpy as np
+import sqlite3
+
+#export LD_LIBRARY_PATH=/home/esztergu/miniconda3/envs/new_SparseChem/lib/:$LD_LIBRARY_PATH
 
 state_dict=torch.load("/home/esztergu/git/SparseChem/examples/chembl/rodent/sc_run_h6000_ldo_r_ldo_t0.8_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.pt")
 w=state_dict['classLast.net.initial_layer.2.weight'].cpu()
@@ -20,19 +23,52 @@ rodent_targets=set(target_all.values.flatten()).difference(target_homo.values.fl
 id_1=0 #first target to compare
 id_2=1
 
-def targets_sim(id_1, id_2):
+def targets_sim(id_1, id_2, eps=1e-15):
     max_sim=0
     best_i= None
     best_j= None
 
     for i in range(id_1*4, (id_1+1)*4):
         for j in range(id_2*4, (id_2+1)*4):
-            tmp=np.dot(w[i],w[j])/(np.linalg.norm(w[i])*np.linalg.norm(w[j]))
+            denom=np.linalg.norm(w[i])*np.linalg.norm(w[j])
+            if denom < eps:
+                continue
+            tmp=np.dot(w[i],w[j])/denom
             #print (f"{i}, {j} -> {tmp}")
             if tmp > max_sim:
                 max_sim = tmp
                 best_i = i
                 best_j = j
 
-    print (f"{best_i}, {best_j} -> {max_sim}")
-    return max_sim    
+    #print (f"{best_i}, {best_j} -> {max_sim}")
+    return max_sim
+
+target_all_list=target_all[0].tolist()
+rodent_index=[i for i in range(len(target_all_list)) if target_all_list[i] in rodent_targets]
+
+target_homo_list=target_homo[0].tolist()
+homo_index=[i for i in range(len(target_all_list)) if target_all_list[i] in target_homo_list]
+
+conn = sqlite3.connect("/home/esztergu/git/chembl-pipeline/input/chembl_29_sqlite/chembl_29.db")
+
+for id_1 in rodent_index:
+    
+    max_sim=0
+    best_homo=None
+    for id_2 in homo_index:
+        sim=targets_sim(id_1, id_2)
+        if sim > max_sim:
+            max_sim = sim
+            best_homo = id_2
+
+    df_prefname_1=pd.read_sql_query("SELECT PREF_NAME,ORGANISM FROM TARGET_DICTIONARY WHERE CHEMBL_ID = ?", conn, params=(target_all_list[id_1],))
+
+    if best_homo is None:
+        print(f"No pair for: {target_all_list[id_1]}({id_1})" )
+    else:
+        df_prefname_2=pd.read_sql_query("SELECT PREF_NAME,ORGANISM FROM TARGET_DICTIONARY WHERE CHEMBL_ID = ?", conn, params=(target_all_list[best_homo],))
+        print(f"Best pair: {target_all_list[id_1]}({id_1})({df_prefname_1['pref_name'][0]}), {target_all_list[best_homo]}({best_homo})({df_prefname_2['pref_name'][0]}), {max_sim}" )
+
+# csv, majd latex table
+
+conn.close()
