0bad1b8a60a84a4b3509b1ffd7bcd64f3bd195a1
Eszter <gulyaseszter.ge@gmail.com>
2024-12-31 16:58:12 +0100

    Partially cleaned

--- target_compare.py
+++ target_compare.py
@@ -4,55 +4,49 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import sqlite3
-sc_path="/home/esztergu/git/SparseChem/examples/chembl/"
+#sc_path="/home/esztergu/git/SparseChem/examples/chembl/"
+sc_path="/home/esztergu/git/Thesis/"
 
 print ("hidden, dropout, weight_decay, auc")
 
-res_human = sc.load_results(sc_path + "models/sc_run_h10000_ldo_r_ldo_t0.7_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #aus=0.811570
+#res_human = sc.load_results(sc_path + "models/sc_run_h10000_ldo_r_ldo_t0.7_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #auc=0.811570
+res_human = sc.load_results(sc_path + "human_only/sc_run_h10000_ldo_r_ldo_t0.7_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #au=0.811570
 
 my_hidden_size = res_human["conf"].hidden_sizes[0]
 my_dropout = res_human["conf"].dropouts_trunk[0]
 my_weight_decay = res_human["conf"].weight_decay
 my_auc_score = res_human["validation"]["classification_agg"].roc_auc_score
+human_auc=my_auc_score
+print (f"Human model: {my_hidden_size}, {my_dropout}, {my_weight_decay}, {my_auc_score}")
 
-print (f"human: {my_hidden_size}, {my_dropout}, {my_weight_decay}, {my_auc_score}")
 
+#res_rodent = sc.load_results(sc_path + "rodent/sc_run_h6000_ldo_r_ldo_t0.8_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #auc=0.801972
+res_multispecies = sc.load_results(sc_path + "multispecies/sc_run_h6000_ldo_r_ldo_t0.8_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #auc=0.801972
 
-res_rodent = sc.load_results(sc_path + "rodent/sc_run_h6000_ldo_r_ldo_t0.8_wd1e-05_lr0.001_lrsteps10_ep20_fva3_fte4.json") #auc=0.801972
+my_hidden_size = res_multispecies["conf"].hidden_sizes[0]
+my_dropout = res_multispecies["conf"].dropouts_trunk[0]
+my_weight_decay = res_multispecies["conf"].weight_decay
+my_auc_score = res_multispecies["validation"]["classification_agg"].roc_auc_score
 
-my_hidden_size = res_rodent["conf"].hidden_sizes[0]
-my_dropout = res_rodent["conf"].dropouts_trunk[0]
-my_weight_decay = res_rodent["conf"].weight_decay
-my_auc_score = res_rodent["validation"]["classification_agg"].roc_auc_score
-
-print (f"rodent: {my_hidden_size}, {my_dropout}, {my_weight_decay}, {my_auc_score}")
+print (f"Multispecies model: {my_hidden_size}, {my_dropout}, {my_weight_decay}, {my_auc_score}")
 
 auc_human=res_human["validation"]["classification"].roc_auc_score
-auc_rodent=res_rodent["validation"]["classification"].roc_auc_score
+auc_multispecies=res_multispecies["validation"]["classification"].roc_auc_score
 
-#df=pd.read_csv("~/git/chembl-pipeline/output/chembl_29/agg_weight.csv")
-#df["rodent_auc"]=auc_rodent
-#auc_rodent_filtered=df[df.aggregation_weight==1.0]["rodent_auc"]
-
-
-#javitani kell
-#plt.scatter(auc_human,auc_rodent)
-#plt.savefig("target_compare.pdf")
 
 folding=np.load("/home/esztergu/git/chembl-pipeline/output/chembl_29/folding_only_human.npy")
-y=np.load("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_thresh_only_human.npy", allow_pickle=True).item(0)
+y_human=np.load("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_thresh_only_human.npy", allow_pickle=True).item(0)
+y_multispecies=np.load("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_thresh_new.npy", allow_pickle=True).item(0)
 
-y_rodent=np.load("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_thresh_new.npy", allow_pickle=True).item(0)
-
+# Selecting all human targets with at least 5 positive and negative samples in all folds (This criteria in SparseChem to compute AUC)
 n=5
-fold_pos, fold_neg = sc.class_fold_counts(y.tocsr(), folding)
+fold_pos, fold_neg = sc.class_fold_counts(y_human.tocsr(), folding)
 aggregation_weight = ((fold_pos >= n).all(0) & (fold_neg >= n)).all(0).astype(np.float64)
-
-# (aggregation_weight*auc_human).sum()/aggregation_weight.sum() 0.811570
+assert np.abs((aggregation_weight*auc_human).sum()/aggregation_weight.sum()-human_auc) < 1e-4, "Computed aggregation weight can not reconstruct model AUC" # 0.811570
 
 auc_human_filtered=auc_human[aggregation_weight==1.0].values
 
-
+# Matching targets by their Chembl ID
 target_all=pd.read_csv("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_targets.csv", header=None)
 target_homo=pd.read_csv("/home/esztergu/git/chembl-pipeline/output/chembl_29/chembl_29_targets_homos.csv", header=None)
 
@@ -71,15 +65,14 @@ target_homo_4_thr=target_homo_4.flatten()+np.array(["_0","_1","_2","_3"]*target_
 selected_targets=set(target_homo_4_thr[aggregation_weight==1.0])
 
 all_mask=[(t in selected_targets) for t in list (target_all_4_thr)]
-auc_rodent_filtered=auc_rodent[all_mask].values #mean-je 0.8112741840355688
+auc_multispecies_filtered=auc_multispecies[all_mask].values #mean-je 0.8112741840355688
 
 all_names=target_all_4_thr[all_mask]
 homo_names=target_homo_4_thr[aggregation_weight==1.0]
 assert (all_names==homo_names).all(), "The two set of targets are different"
 
 
-
-dif=auc_rodent_filtered-auc_human_filtered
+dif=auc_multispecies_filtered-auc_human_filtered
 #dif.argmax()
 # 138
 # all_names[138]
@@ -88,8 +81,8 @@ top10=dif.argsort()[-10:]
 top30=dif.argsort()[-30:]
 
 plt.cla()
-plt.scatter(auc_human_filtered,auc_rodent_filtered,s=1)
-plt.scatter(auc_human_filtered[top10],auc_rodent_filtered[top10],c="red",s=2)
+plt.scatter(auc_human_filtered,auc_multispecies_filtered,s=1)
+plt.scatter(auc_human_filtered[top10],auc_multispecies_filtered[top10],c="red",s=2)
 plt.plot([0,1],[0,1],c="red")
 plt.savefig("target_compare_top10.pdf")
 
