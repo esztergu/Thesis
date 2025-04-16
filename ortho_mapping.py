a8c3426c19ba12bd4191c901da93c6babd997efa
Eszter <gulyaseszter.ge@gmail.com>
2025-04-16 16:36:26 +0200

    Refactoring for efficient memory usage

--- ortho_mapping.py
+++ ortho_mapping.py
@@ -13,24 +13,40 @@ for i in target_all.to_numpy():
         df_list.append(df)
 
 df = pd.concat(df_list)
+df_list = None
 
-print(df)
-
+human_targets=set(target_homo.values.flatten())
 rodent_targets=set(target_all.values.flatten()).difference(target_homo.values.flatten())
-rodent_uni = df[df["chembl_id"].isin(rodent_targets)]
 
-orthoUni = pd.read_csv("/home/aarany/odb12v0_gene_uniprot_only.tab", sep="\t", header=None)
-orthoUni = orthoUni.rename(columns={1:"accession"})
-ortho_mapping = pd.merge(orthoUni, rodent_uni, on="accession")
-orthoUni = None
+# Creating ortho group mapping for rodent and human targets
+rodent_uni = df[df["chembl_id"].isin(rodent_targets)]
+human_uni = df[df["chembl_id"].isin(human_targets)]
+
+orthoUni = pd.read_csv("orthoDB/odb12v0_gene_uniprot_only.tab", sep="\t", header=None)
+orthoUni = orthoUni.rename(columns={0:"gene", 1:"accession"})[["gene", "accession"]]
+
+rodent_ortho_mapping = pd.merge(orthoUni, rodent_uni, on="accession")
+human_ortho_mapping = pd.merge(orthoUni, human_uni, on="accession")
+orthoUni = None # To save memory
 
 OG2genes = pd.read_csv("orthoDB/odb12v0_OG2genes.tab", sep="\t", header=None)
-OG2genes = OG2genes.rename(columns={1:"gene"})
-ortho_mapping = ortho_mapping.rename(columns={0:"gene"})
-OG_mapping = pd.merge(ortho_mapping, OG2genes, on="gene")
-# OG_mapping = OG_mapping.rename(columns={4:"OrthoGroup"}) ??? hanyadik oszlop
-OG_mapping.to_csv("OG_mapping.csv")
-OG_mapping = OG_mapping[["chembl_id", "OrthoGroup"]]
+OG2genes = OG2genes.rename(columns={0:"OrthoGroup", 1:"gene"})
 
+rodent_OG_mapping = pd.merge(rodent_ortho_mapping, OG2genes, on="gene")
+rodent_OG_mapping = rodent_OG_mapping[["chembl_id", "OrthoGroup"]]
+rodent_OG_mapping.to_csv("rodent_OG_mapping.csv")
 
+human_OG_mapping = pd.merge(human_ortho_mapping, OG2genes, on="gene")
+OG2genes = None
+human_OG_mapping = human_OG_mapping[["chembl_id", "OrthoGroup"]]
+human_OG_mapping.to_csv("human_OG_mapping.csv")
 
+rodent_to_human = pd.merge(rodent_OG_mapping, human_OG_mapping, on="OrthoGroup", suffixes=("_rodent", "_human"))
+rodent_to_human = rodent_to_human[["chembl_id_rodent", "chembl_id_human"]].drop_duplicates()
+rodent_to_human.to_csv("ortholog_mapping.csv")
+
+namemapping=pd.read_sql_query("SELECT PREF_NAME, CHEMBL_ID FROM TARGET_DICTIONARY", conn)
+temp=pd.merge(rodent_to_human, namemapping, left_on="chembl_id_rodent", right_on="chembl_id") #Rename x and y
+temp=temp.rename(columns={"pref_name":"pref_name_rodent"})[["chembl_id_rodent", "chembl_id_human", "pref_name_rodent"]]
+rodent_to_human_names=pd.merge(temp, namemapping, left_on="chembl_id_human", right_on="chembl_id").rename(columns={"pref_name":"pref_name_human"})
+rodent_to_human_names.to_csv("ortholog_mapping_with_names.csv")
