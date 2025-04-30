d7a82d7c8ff97cf4e86f39d423c93e97486633c8
Eszter <gulyaseszter.ge@gmail.com>
2025-04-30 14:34:51 +0200

    Final cleaning up of the comments

--- filter_compunds.py
+++ filter_compunds.py
@@ -4,42 +4,33 @@ import scipy as sp
 import sparsechem as sc
 chembl_path="dependencies/EszterGulyasBSc/chembl-pipeline-main/output/chembl_29/"
 
+# Opening the required files
 folding = np.load(chembl_path+"folding.npy")
 y = np.load(chembl_path+"chembl_29_thresh.npy",allow_pickle=True).item(0)
-# <1443601x10296 sparse matrix of type '<class 'numpy.float64'>'with 31167071 stored elements in COOrdinate format>
-targets = pd.read_csv(chembl_path+"chembl_29_targets.csv",header=None) # 2574
-targets_h = pd.read_csv(chembl_path+"chembl_29_targets_homos.csv",header=None) #2526
+targets = pd.read_csv(chembl_path+"chembl_29_targets.csv",header=None)
+targets_h = pd.read_csv(chembl_path+"chembl_29_targets_homos.csv",header=None)
 
 targets_h["is_homo"] = 1
 
 joined = targets.join(targets_h.set_index(0), on=0, how="outer")
-is_homo = np.array(~joined['is_homo'].isnull()) # True ahol homo, 48 False
-mask = np.abs(y) # -1: inaktiv 1: aktiv 0: nincs lemérve; ebből kell 1: inaktiv vagy aktiv 0: nincs lemérve
+is_homo = np.array(~joined['is_homo'].isnull()) # Mask which is True for homo sapiens targets
+mask = np.abs(y) # y: -1: inactive, 1: active, 0: unmeasured -> mask: 1: measured, 0: unmeasured
 # Repeating 4 times for the thresholds
-is_homo= np.repeat(is_homo,4) # matrix korrekció
-homo_count = mask @ is_homo # matrix szorzás
-# homo_count.shape
-# sum(homo_count == 0) 4743 csak egér
-# print(sum(homo_count == 0))
+is_homo = np.repeat(is_homo,4)
+homo_count = mask @ is_homo
 is_only_rodent = (homo_count == 0)
 folding_3_or_4 = (folding == 3) | (folding == 4)
-keep = ~(is_only_rodent & folding_3_or_4) # nem csak rodent vagy nem a folding 3 és 4-ben van
-# sum(keep) 1441631
-# keep.shape (1443601,) 1970 vegyületet dobtunk el, ez marad a folding 3 és 4-re, ami jó, sok maradt tanulni
+keep = ~(is_only_rodent & folding_3_or_4)
 keep_only_human = ~(is_only_rodent)
 
 y_csr = y.tocsr()
-# y_csr
-# <1443601x10296 sparse matrix of type '<class 'numpy.float64'>' with 31167071 stored elements in Compressed Sparse Row format>
 y_new = y_csr[keep]
-# <1441631x10296 sparse matrix of type '<class 'numpy.float64'>' with 31157117 stored elements in Compressed Sparse Row format>
 folding_new = folding[keep]
-# folding_new.shape (1441631,)
 
+# Saving the filtered results
 np.save(chembl_path+"folding_new.npy",folding_new)
 np.save(chembl_path+"chembl_29_thresh_new.npy",y_new)
 x = np.load(chembl_path+"chembl_29_X.npy",allow_pickle=True).item(0)
-# x.shape (1443601, 32000)
 x_new = x[keep]
 np.save(chembl_path+"chembl_29_X_new.npy",x_new)
 
